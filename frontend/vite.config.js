879509ba26991ad82785d199d89318352953ae32
bastien <bastien@MacBook-de-Bast.local>
2024-10-16 14:45:44 +0200

    feat: build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build'  // Ensure this matches the distDir in your vercel.json
+    outDir: 'build'
   }
 })
